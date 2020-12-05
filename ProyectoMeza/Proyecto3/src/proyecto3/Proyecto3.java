16d9a190426c352ef57c59519aaa121f6c293a6b
Shari Andr <al227832@edu.uaa.mx>
2020-12-04 21:13:47 -0600

    ahorrando código

--- ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
+++ ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
@@ -59,35 +59,16 @@ public class Proyecto3 {
     public static void main(String[] args) {
         boolean salir=false;
         Scanner sc=null; 
+        Proyecto3 grafo=null;
         try{
-            sc = new Scanner(System.in);
-            System.out.println("Inserte el número de vértices del grafo: ");
-            int v=sc.nextInt();
-            Proyecto3 grafo = new Proyecto3(v);
-            for (int i=0;i<v; i++){
-                System.out.println("Inserte el número de aristas del vértice "+(i+1)+": ");
-                int e=sc.nextInt();
-                for (int j=0; j<e; j++){
-                    System.out.println("Inserte el vértice al que se conecta la arista "+(j+1)+": ");
-                    int arista=sc.nextInt();
-                    grafo.add(i, arista);
-                }
-            }
-
-        while(!salir){
-            System.out.println("Menú");
-            System.out.println("1) Crear nuevo grafo");
-            System.out.println("2) Imprimir matriz y lista de adyacencia");
-            System.out.println("... ");
-            System.out.println("0) Salir ");
-            char op=sc.next().charAt(0);
-            switch(op){
-                case('1'):
+            while(!salir){
+                sc = new Scanner(System.in);
+                if(grafo==null){
                     System.out.println("Inserte el número de vértices del grafo: ");
-                    v=sc.nextInt();
+                    int v=sc.nextInt();
                     grafo = new Proyecto3(v);
                     for (int i=0;i<v; i++){
-                        System.out.println("Inserte el número de aristas del vértice "+(i)+": ");
+                        System.out.println("Inserte el número de aristas del vértice "+(i+1)+": ");
                         int e=sc.nextInt();
                         for (int j=0; j<e; j++){
                             System.out.println("Inserte el vértice al que se conecta la arista "+(j+1)+": ");
@@ -95,18 +76,28 @@ public class Proyecto3 {
                             grafo.add(i, arista);
                         }
                     }
-                break;
-                case('2'):
-                    grafo.MostrarMatriz();
-                    grafo.MostrarLista();
-                break;
-                // ...
-                case('0'):
-                    salir=true;
-                break;
-            }
-        }
+                }
 
+                System.out.println("Menú");
+                System.out.println("1) Crear nuevo grafo");
+                System.out.println("2) Imprimir matriz y lista de adyacencia");
+                System.out.println("... ");
+                System.out.println("0) Salir ");
+                char op=sc.next().charAt(0);
+                switch(op){
+                    case('1'):
+                        grafo=null;
+                    break;
+                    case('2'):
+                        grafo.MostrarMatriz();
+                        grafo.MostrarLista();
+                    break;
+                    // ...
+                    case('0'):
+                        salir=true;
+                    break;
+                }
+            }
         }
         catch(Exception e){
             System.out.println(e);
@@ -114,14 +105,8 @@ public class Proyecto3 {
             if(sc!=null)
                 sc.close();
                 System.out.println("Cerrando programa...");
-        }
-        
-        
+        }      
     }
-    
-    
-        
-
 }
     
 
