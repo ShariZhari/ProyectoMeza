bff219ec1b2eaad1e12056726f98337fd8fbafd0
Shari Andr <al227832@edu.uaa.mx>
2020-12-04 20:14:50 -0600

    menu

--- ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
+++ ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
@@ -55,18 +55,73 @@ public class Proyecto3 {
         System.out.println("");
     }
     
+
     public static void main(String[] args) {
-        // ´porque
+        boolean salir=false;
+        Scanner sc=null; 
+        try{
+            sc = new Scanner(System.in);
+            System.out.println("Inserte el número de vértices del grafo: ");
+            int v=sc.nextInt();
+            Proyecto3 grafo = new Proyecto3(v);
+            for (int i=0;i<v; i++){
+                System.out.println("Inserte el número de aristas del vértice "+(i+1)+": ");
+                int e=sc.nextInt();
+                for (int j=0; j<e; j++){
+                    System.out.println("Inserte el vértice al que se conecta la arista "+(j+1)+": ");
+                    int arista=sc.nextInt();
+                    grafo.add(i, arista);
+                }
+            }
+
+        while(!salir){
+            System.out.println("Menú");
+            System.out.println("1) Crear nuevo grafo");
+            System.out.println("2) Imprimir matriz y lista de adyacencia");
+            System.out.println("... ");
+            System.out.println("0) Salir ");
+            char op=sc.next().charAt(0);
+            switch(op){
+                case('1'):
+                    System.out.println("Inserte el número de vértices del grafo: ");
+                    v=sc.nextInt();
+                    grafo = new Proyecto3(v);
+                    for (int i=0;i<v; i++){
+                        System.out.println("Inserte el número de aristas del vértice "+(i)+": ");
+                        int e=sc.nextInt();
+                        for (int j=0; j<e; j++){
+                            System.out.println("Inserte el vértice al que se conecta la arista "+(j+1)+": ");
+                            int arista=sc.nextInt();
+                            grafo.add(i, arista);
+                        }
+                    }
+                break;
+                case('2'):
+                    grafo.MostrarMatriz();
+                    grafo.MostrarLista();
+                break;
+                // ...
+                case('0'):
+                    salir=true;
+                break;
+            }
+        }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }finally{
+            if(sc!=null)
+                sc.close();
+                System.out.println("Cerrando programa...");
+        }
+        
         
     }
     
-    public void ok(){
-        //es una prueba
-        //desde vscode :)
-        //aaaaaaah
-        //yay
-        //henlo
-        //nada IMANOLlllll
+    
+        
+
 }
     
-}
+
