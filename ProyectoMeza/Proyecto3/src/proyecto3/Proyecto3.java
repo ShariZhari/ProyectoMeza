be4266374c6e06f6b75e91ce448fa050e5ac8970
Shari Andr <al227832@edu.uaa.mx>
2020-12-09 21:11:19 -0600

    pareos

--- ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
+++ ProyectoMeza/Proyecto3/src/proyecto3/Proyecto3.java
@@ -1,13 +1,68 @@
 package proyecto3;
 import java.util.*;  
-import javax.swing.JOptionPane;
+
+/*
+    explicación - por favor borren esto después de leerlo
+       -Cuando se crea el grafo, se creará también un ArrayList<par> llamado TotalAxV, que
+        guardará cuántas aristas tiene cada vértice.
+
+       -Para esto necesitaremos la clase par, que permite guardar dos enteros y comparar los
+        objetos base al primer entero (tuve que crearla porque en java tengo que importar una
+        librería medio rara mientras que en c++ no es necesario tanto rollo jajaja)
+        por lo que se guardara el total de aristas en el primer espacio, y el vértice al que
+        pertenecen en el segundo espacio. 
+        
+       -El objetivo es iniciar el pareo a partir de los vértices que tienen un menor número de 
+        aristas, y de esta forma obtener el máximo posible de vértices; así que ordenaremos el
+        ArrayList en base al total de aristas, e iremos analizando vértice por vértice en la 
+        función ActivarPareo(), en dicho orden.
+
+       -Despúes, en Pareo(), revisaremos si ya están emparejados los vértices de destino de 
+        las aristas de nuestro vértice de origen, basado en nuestro arreglo de pares, el cual
+        guarda los vértices con los que está emparejado cada nodo.
+        
+       -Si, al emparejar un nodo (llamémoslo origen), se obtiene que el vértice al que apunta 
+        su arista (llamémoslo destino) ya está emparejado con otro nodo:
+            1.- Se tomará otra arista del vértice origen y se buscará si algún otro vértice 
+                adyacente no está apareado.
+            2.- Si el vértice origen ya no tiene más aristas, ahora se buscará si el nodo 
+                destino puede ser emparejado con otro vértice. Si se logra hacer este cambio,
+                se volverá a llamar a la función Pareo() con el vértice original, para que 
+                ahora sí pueda ser apareado.
+
+       -Este proceso se realiza antes de que el usuario elija la opción de los pareos del grafo,
+        para que no se tenga que repetir el mismo procedimiento cuando quiera ver si el grafo
+        es perfecto o no.
+        (Igual se puede poner en un mismo inciso xd)
+*/
+
+class par //permitirá asociar a cada vértice con su total de aristas
+{
+    private int first; 
+    private int second;
+
+    par(int first, int second){
+        this.first=first;
+        this.second=second;
+    }
+    public int getFirst() {
+        return first;
+    }
+    public int getSecond() {
+        return second;
+    }
+    public static int comparar(par uno, par dos){
+        return Integer.compare(uno.first,dos.first);
+    }
+}
 
 public class Proyecto3 
 {
     private int n; //Número de vértices
     private int matriz[][];
     private LinkedList <Integer> lista[];
-    
+    private int pares[];
+
     private static boolean grafoCreado = false;
     private static Scanner sc = new Scanner(System.in);
     private static Proyecto3 grafo;
@@ -17,10 +72,12 @@ public class Proyecto3
         this.n = n;
         matriz = new int[n][n];
         lista = new LinkedList[n];
+        pares=new int[n];
         
         for(int i=0;i<n;i++)
         {
             lista[i] = new LinkedList();
+            pares[i]=-1;
             
             for(int j = 0;j < n; j++)
             {
@@ -78,7 +135,7 @@ public class Proyecto3
             System.out.println("Inserte el número de vértices del grafo: ");
             int v = sc.nextInt(); //Número de vértices
             grafo = new Proyecto3(v);
-            
+            ArrayList<par> TotalAxV = new ArrayList<par>(v);
             for (int i = 0;i < v; i++)
             {
                 System.out.println("Inserte el número de aristas del vértice " + i + ": ");
@@ -89,7 +146,10 @@ public class Proyecto3
                     int arista = sc.nextInt();
                     grafo.add(i, arista);
                 }
+                TotalAxV.add(i, new par(e,i)); //se guarda el numero total de aristas que tiene cada vértice
             }
+            TotalAxV.sort(par::comparar); //se ordenan los vértices del menor al mayor número de aristas
+            grafo.ActivarPareo(TotalAxV);
             grafoCreado = true;
         } catch (Exception e) 
         {
@@ -207,23 +267,96 @@ public class Proyecto3
         }
     }
     
+    void ImprimirPareos(boolean prf)
+    {
+        boolean perfecto=true;
+        System.out.println("Pareo de nodos");
+        for(int i = 0;i < n; i++)
+        {   
+            if(pares[i]==-1){
+                System.out.println(i + " no tiene par");
+                perfecto = false;
+            }
+            else
+                System.out.println(i + " y " + pares[i]);
+        }
+        System.out.println("");
+        if(prf){
+            if(perfecto)
+                System.out.println("El grafo sí tiene pareos perfectos, pues, al emparejar la mayor cantidad \nposible de vértices, todos los vértices del grafo quedan emparejados.");
+            else
+                System.out.println("El grafo no tiene pareos perfectos, pues, al emparejar la mayor cantidad \nposible de vértices, no todos los vértices del grafo quedan emparejados.");
+            }
+    }
+
+
+    boolean Pareo(int origen, int cambio){
+        Iterator<Integer> i = lista[origen].listIterator(); 
+        while(i.hasNext()) 
+        {//itera por todas las aristas del vértice de origen hasta hallar un vértice de destino que no este apareado
+            int destino=i.next();
+                if(pares[origen]==-1)
+                {
+                    if(destino==cambio) //cambio determina si dicha arista ya había sido usada en un pareo anterior de ese vértice
+                    { //se ignorará esta arista si se ha llamado a la función con el objetivo de cambiar el pareo de un vertice que ya había sido apareado anteriormente
+                        continue;
+                    }
+                    if(pares[destino]==-1)
+                    { //si ambos vértices de la arista no han sido apareados, se juntarán
+                        pares[origen]=destino;
+                        pares[destino]=origen;
+                        return true; //y la función termina después de crear un pareo exitoso  
+                    } 
+                    else
+                    { //si el vértice de destino ya está apareado
+                        if(i.hasNext())
+                            continue;   //iterar sobre los demás vértices
+                        else if (lista[destino].size()>1)
+                        { //si no hay más vértices adyacentes en el vértice de origen, buscar si en el vértice de destino hay otros vértices con los que pueda aparearse
+                            int aux = pares[destino];
+                            pares[aux]=-1; //se deshace el pareo
+                            pares[destino]=-1;
+                            if (Pareo(destino, aux)) //si se logró un cambio...
+                                Pareo(origen, -1); //entonces ya podremos aparear a nuestro vértice original con el vértice de destino original
+                        }
+                }
+            }  
+        }
+        return false;
+    }
+
+    void ActivarPareo(ArrayList<par>TotalAxV)
+    {
+        for(int i = 0;i < n; i++)
+        {
+            int numAristas= TotalAxV.get(i).getFirst(); //iniciamos con los vértices de menor número de aristas
+            int vert= TotalAxV.get(i).getSecond();
+            if(numAristas>0){
+                Pareo(vert,-1);
+            }
+
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException 
     {
         boolean salir = false; 
         
         while(!salir)
         {
-            System.out.println("Menú");
+            System.out.println("\nMenú");
             System.out.println("1) Crear nuevo grafo");
             System.out.println("2) Imprimir matriz y lista de adyacencia");
             System.out.println("3) Recorrido en profundidad (DFS)");
             System.out.println("4) Recorrido en anchura (BFS)");
             System.out.println("5) Problema con uso de grafos dirigidos");
+            System.out.println("6) Determinar si el grafo tiene pareos perfectos");
+            System.out.println("7) Obtener el pareo maximal");
             System.out.println("... ");
-            System.out.println("7) Salir ");
+            System.out.println("9) Salir ");
             System.out.print("Opción: ");
             char op = sc.next().charAt(0);
-
+            System.out.println("");
             switch(op)
             {
                 case('1'):
@@ -285,7 +418,7 @@ public class Proyecto3
                     System.out.println("\nSuponga que a lo largo de su colonia, usted ha ubicado 5 puntos estratégicos, digamos: una tienda de ropa"
                             + ", \nun puesto de comida, una papelería, una escuela y, por supuesto, su hogar. Ahora bien, con el apogeo de las plata-"
                             + "\nformas de entrega de comida a domicilio (dada la situación actual), usted también se ha visto en la necesidad, o el"
-                            + "\nsimple deseo, de ordenar comida a su hogar. No obstante, los repartidores suelen enfrentar un problema que cada vez"
+                            + "\nsimple deseo, de ordenar comida a su hogar. No auxante, los repartidores suelen enfrentar un problema que cada vez"
                             + "\nparece más común: Google Maps con frecuencia marca calles en sentidos contrarios. Ante esta situación, usted ha deci"
                             + "\ndido apoyar a los repartidores indicándoles la manera en que pueden atravesar su colonia, de lado a lado, a partir de"
                             + "\nlos sitios característicos, sin incurrir en calles en sentido contrario, lo que los ayuda a prevenir tomar parte de un"
@@ -309,7 +442,30 @@ public class Proyecto3
                     }
                     break;
                 }
+                case('6'):
+                {
+                    if(!grafoCreado)
+                    {
+                        System.out.println("¡Debes crear un grafo primero!");
+                    }else
+                    {
+                        grafo.ImprimirPareos(true);
+                    }  
+                    break;
+                }
                 case('7'):
+                {
+                    if(!grafoCreado)
+                    {
+                        System.out.println("¡Debes crear un grafo primero!");
+                    }else
+                    {
+                        grafo.ImprimirPareos(false);
+                    }
+                        
+                    break;
+                }
+                case('9'):
                 {
                     salir = true;
                     break;
